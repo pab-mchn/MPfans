f25e97b0b2ea85ddc9369d0e9c8f23af023fe480
Pablo <Pablo@Pablos-MBP.local>
2025-05-26 11:15:22 +0200

    deploy configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
+  basePath: '/MPfans',
+  assetPrefix: '/MPfans/',
 };
+
+export default nextConfig;
